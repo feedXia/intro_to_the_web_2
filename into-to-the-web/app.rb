fb0adcfd00edd0a76b918786996e7ff3c08e34d1
Xia <zeyaoliu@me.com>
2021-10-05 16:32:35 +0100

    Added cat names! :)

--- into-to-the-web/app.rb
+++ into-to-the-web/app.rb
@@ -21,7 +21,6 @@ get "/the-plague" do
 end
 
 get "/cat" do
-  "<div style='border: dashed red'>
-    <img src='https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip'>
-  </div>"
+  @cat_names = ["Amigo", "Misty", "Almond", "Matthew", "Xia"].sample
+  erb(:index)
 end
