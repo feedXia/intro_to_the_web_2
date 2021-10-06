0919a220164e28446b3cc0909711971a1a00e57a
Xia <zeyaoliu@me.com>
2021-10-06 15:59:30 +0100

    Added lib file to setup rackup

--- into-to-the-web/app.rb
+++ into-to-the-web/app.rb
@@ -4,35 +4,37 @@ require "sinatra/reloader" if development?
 # set :port, 1234
 # set :bind, "0.0.0.0"
 
-get "/" do
-  "hello!"
-end
+class IntroToWeb < Sinatra::Base
+  get "/" do
+    "hello!"
+  end
 
-get "/secret" do
-  "We are serious developers"
-end
+  get "/secret" do
+    "We are serious developers"
+  end
 
-get "/harry-potter" do
-  "You're a wizard Harry"
-end
+  get "/harry-potter" do
+    "You're a wizard Harry"
+  end
 
-get "/the-plague" do
-  "Under the vast indifference of the sky"
-end
+  get "/the-plague" do
+    "Under the vast indifference of the sky"
+  end
 
-get "/random-cat" do
-  @cat_names = ["Amigo", "Misty", "Almond", "Matthew", "Xia"].sample
-  erb(:index)
-end
+  get "/random-cat" do
+    @cat_names = ["Amigo", "Misty", "Almond", "Matthew", "Xia"].sample
+    erb(:index)
+  end
 
-post "/named-cat" do
-  p params
-  @cat_names = params[:name]
-  erb :index
-end
+  post "/named-cat" do
+    p params
+    @cat_names = params[:name]
+    erb :index
+  end
 
-get "/cat_form" do
-  erb :cat_form
-end
+  get "/cat_form" do
+    erb :cat_form
+  end
 
-run! if app_file == $0
+  run! if app_file == $0
+end
